5ba1bf518fc2286cf0dce0e58bc6d44e21a86812
techtwins <gshevaughn@gmail.com>
2020-11-02 22:13:22 -0500

    done

--- app/controllers/ducks_controller.rb
+++ app/controllers/ducks_controller.rb
@@ -3,5 +3,33 @@ class DucksController < ApplicationController
   def index
     @ducks = Duck.all
   end
+  
+  def show
+    @duck = Duck.find(params[:id])
+  end
+  
+  def new
+    @duck = Duck.new
+  end
+  
+  def edit
+    @duck = Duck.find(params[:id])
+  end
 
+  def create
+    @duck = Duck.create(duck_params)
+    redirect_to duck_path(@duck)
+  end
+
+  def update
+    @duck = Duck.find(params[:id])
+    @duck.update(duck_params)
+    redirect_to duck_path(@duck)
+  end
+
+  private
+
+  def duck_params
+    params.require(:duck).permit(:name, :description, :student_id)
+  end
 end
--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -3,5 +3,33 @@ class StudentsController < ApplicationController
   def index
     @students = Student.all
   end
+  
+  def show
+    @student = Student.find(params[:id])
+  end
+  
+  def new
+    @student = Student.new
+  end
+  
+  def edit
+    @student = Student.find(params[:id])
+  end
 
+  def create
+    @student = Student.create(duck_params)
+    redirect_to student_path(@student)
+  end
+
+  def update
+    @student = Student.find(params[:id])
+    @student.update(duck_params)
+    redirect_to student_path(@student)
+  end
+
+  private
+
+  def duck_params
+    params.require(:student).permit(:name, :mod)
+  end
 end
